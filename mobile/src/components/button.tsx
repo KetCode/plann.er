07ae21de5a54aece291bdc7bdf8d1fb66d8162b9
KesseT <64742407+KetCode@users.noreply.github.com>
2024-07-12 20:26:59 -0300

    update: fix button background

--- mobile/src/components/button.tsx
+++ mobile/src/components/button.tsx
@@ -1,6 +1,7 @@
 import { createContext, useContext } from "react"
 
 import {
+  View,
   Text,
   TextProps,
   TouchableOpacity,
@@ -22,26 +23,25 @@ function Button({
   variant = "primary",
   children,
   isLoading,
-  className,
   ...rest
 }: ButtonProps) {
   return (
     <TouchableOpacity
-      className={clsx(
-        "h-11 flex-row items-center justify-center rounded-lg gap-2 px-2",
-        {
-          "bg-lime-300": variant === "primary",
-          "bg-zinc-800": variant === "secondary",
-        },
-        className
-      )}
       activeOpacity={0.7}
       disabled={isLoading}
       {...rest}
     >
-      <ThemeContext.Provider value={{ variant }}>
-        {isLoading ? <ActivityIndicator className="text-lime-950" /> : children}
-      </ThemeContext.Provider>
+      <View className={clsx(
+        "h-11 flex-row items-center justify-center rounded-lg gap-2 px-2",
+        {
+          "bg-lime-300": variant === "primary",
+          "bg-zinc-800": variant === "secondary",
+        }
+      )}>
+        <ThemeContext.Provider value={{ variant }}>
+          {isLoading ? <ActivityIndicator className="text-lime-950" /> : children}
+        </ThemeContext.Provider>
+      </View>
     </TouchableOpacity>
   )
 }
