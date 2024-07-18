ec4f8a4573d28c5afa0d367c2ca9b1ab14e7b345
KesseT <64742407+KetCode@users.noreply.github.com>
2024-07-18 12:46:12 -0300

    test: registry and find links from backend

---
+++ server/src/models/repositories/links_repository_test.py
@@ -0,0 +1,33 @@
+import uuid
+import pytest
+
+from .links_repository import LinksRepository
+from src.models.settings.db_connection_handler import db_connection_handler
+
+db_connection_handler.connect()
+
+trip_id = str(uuid.uuid4())
+
+@pytest.mark.skip(reason="interação com banco")
+def test_registry_link():
+    conn = db_connection_handler.get_connection()
+    link_repository = LinksRepository(conn)
+
+    link_infos = {
+        "id": str(uuid.uuid4()),
+        "trip_id": trip_id,
+        "link": "teste.com",
+        "title": "Teste",
+    }
+
+    link_repository.registry_link(link_infos)
+
+@pytest.mark.skip(reason="interação com banco")
+def test_find_links_from_trip():
+    conn = db_connection_handler.get_connection()
+    link_repository = LinksRepository(conn)
+
+    links = link_repository.find_links_from_trip(trip_id)
+    
+    assert isinstance(links, list)
+    assert isinstance(links[0], tuple)
