34001b69de2ee84242a01bfc448958ad5599761e
KesseT <64742407+KetCode@users.noreply.github.com>
2024-07-18 11:46:16 -0300

    test: update trip status method

--- server/src/models/repositories/trips_repository_test.py
+++ server/src/models/repositories/trips_repository_test.py
@@ -1,4 +1,5 @@
 import uuid
+import pytest
 
 from datetime import datetime, timedelta
 from .trips_repository import TripsRepository
@@ -8,6 +9,7 @@ db_connection_handler.connect()
 
 trip_id = str(uuid.uuid4())
 
+@pytest.mark.skip(reason="interação com banco")
 def test_create_trip():
     conn = db_connection_handler.get_connection()
     trips_repository = TripsRepository(conn)
@@ -23,8 +25,16 @@ def test_create_trip():
 
     trips_repository.create_trip(trips_infos)
 
+@pytest.mark.skip(reason="interação com banco")
 def test_find_trip_by_id():
     conn = db_connection_handler.get_connection()
     trips_repository = TripsRepository(conn)
 
     trip = trips_repository.find_trip_by_id(trip_id)
+
+@pytest.mark.skip(reason="interação com banco")
+def test_update_trip_status():
+    conn = db_connection_handler.get_connection()
+    trips_repository = TripsRepository(conn)
+
+    trips_repository.update_trip_status(trip_id)
