24bd31f1843bdec2046bc9f395bd93629df823f3
KesseT <64742407+KetCode@users.noreply.github.com>
2024-07-18 11:22:59 -0300

    test: trip by id method

--- server/src/models/repositories/trips_repository_test.py
+++ server/src/models/repositories/trips_repository_test.py
@@ -6,12 +6,14 @@ from src.models.settings.db_connection_handler import db_connection_handler
 
 db_connection_handler.connect()
 
+trip_id = str(uuid.uuid4())
+
 def test_create_trip():
     conn = db_connection_handler.get_connection()
     trips_repository = TripsRepository(conn)
 
     trips_infos = {
-        "id": str(uuid.uuid4()),
+        "id": trip_id,
         "destination": "Recife",
         "start_date": datetime.strptime("02-01-2023", "%d-%m-%Y"),
         "end_date": datetime.strptime("02-01-2023", "%d-%m-%Y") + timedelta(days=5),
@@ -20,3 +22,9 @@ def test_create_trip():
     }
 
     trips_repository.create_trip(trips_infos)
+
+def test_find_trip_by_id():
+    conn = db_connection_handler.get_connection()
+    trips_repository = TripsRepository(conn)
+
+    trip = trips_repository.find_trip_by_id(trip_id)
