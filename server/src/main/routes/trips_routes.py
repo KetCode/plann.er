4731e54cf11fd0b5e9db5d8dc7637a2bd997da7c
KesseT <64742407+KetCode@users.noreply.github.com>
2024-07-19 09:21:13 -0300

    imp: confirm trip route

--- server/src/main/routes/trips_routes.py
+++ server/src/main/routes/trips_routes.py
@@ -2,9 +2,12 @@ from flask import jsonify, Blueprint, request
 
 from src.controllers.trip_creator import TripCreator
 from src.controllers.trip_finder import TripFinder
+from src.controllers.trip_confirmer import TripConfirmer
+from src.controllers.link_creator import LinkCreator
 
 from src.models.repositories.trips_repository import TripsRepository
 from src.models.repositories.emails_to_invite_repository import EmailsToInviteRepository
+from src.models.repositories.links_repository import LinksRepository
 
 from src.models.settings.db_connection_handler import db_connection_handler
 
@@ -28,5 +31,15 @@ def find_trip(tripId):
     controller = TripFinder(trips_repository)
 
     response = controller.find_trip_details(tripId)
+
+    return jsonify(response["body"]), response["status_code"]
+
+@trips_routes_bp.route("/trips/<tripId>/confirm", methods=["GET"])
+def confirm_trip(tripId):
+    conn = db_connection_handler.get_connection()
+    trips_repository = TripsRepository(conn)
+    controller = TripConfirmer(trips_repository)
+
+    response = controller.confirm(tripId)
 
     return jsonify(response["body"]), response["status_code"]
