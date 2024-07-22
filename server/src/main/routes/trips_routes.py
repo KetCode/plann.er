4371335739dfba6e335ab438f6593e8c64b5f19c
KesseT <64742407+KetCode@users.noreply.github.com>
2024-07-21 21:07:34 -0300

    imp: find trip route

--- server/src/main/routes/trips_routes.py
+++ server/src/main/routes/trips_routes.py
@@ -4,6 +4,7 @@ from src.controllers.trip_creator import TripCreator
 from src.controllers.trip_finder import TripFinder
 from src.controllers.trip_confirmer import TripConfirmer
 from src.controllers.link_creator import LinkCreator
+from src.controllers.link_finder import LinkFinder
 
 from src.models.repositories.trips_repository import TripsRepository
 from src.models.repositories.emails_to_invite_repository import EmailsToInviteRepository
@@ -51,5 +52,15 @@ def create_trip_link(tripId):
     controller = LinkCreator(links_repository)
 
     response = controller.create(request.json, tripId)
+
+    return jsonify(response["body"]), response["status_code"]
+
+@trips_routes_bp.route("/trips/<tripId>/link", methods=["GET"])
+def find_trip_link(tripId):
+    conn = db_connection_handler.get_connection()
+    links_repository = LinksRepository(conn)
+    controller = LinkFinder(links_repository)
+
+    response = controller.find(tripId)
 
     return jsonify(response["body"]), response["status_code"]
