5721dc235acd1cfb1e2c1d6bbc4c747599272e67
KesseT <64742407+KetCode@users.noreply.github.com>
2024-12-28 15:04:10 -0300

    imp: count emails invited

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -93,9 +93,13 @@ export function App() {
 
           {isGuestsInputOpen && (
             <div className="h-16 bg-zinc-900 px-4 rounded-xl flex items-center shadow-shape gap-3">
-              <button type="button" onClick={openGuestsModal} className="flex items-center gap-2 flex-1">
+              <button type="button" onClick={openGuestsModal} className="flex items-center gap-2 flex-1 text-left">
                 <UserRoundPlus className='size-5 text-zinc-400' />
-                <span className="text-lg text-zinc-400 flex-1 text-left">Quem estará na viagem?</span>
+                {emailsToInvite.length > 0 ? (
+                  <span className="text-lg text-zinc-100 flex-1">{emailsToInvite.length} pessoa(s) convidada(s)</span>
+                ): (
+                  <span className="text-lg text-zinc-400 flex-1">Quem estará na viagem?</span>
+                )}
               </button>
 
               <div className='w-px h-6 bg-zinc-800' />
