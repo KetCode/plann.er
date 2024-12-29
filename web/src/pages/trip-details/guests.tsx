08266dd60aab8904760fb2973a226ae027090e06
KesseT <64742407+KetCode@users.noreply.github.com>
2024-12-29 12:16:56 -0300

    ref: button component

--- web/src/pages/trip-details/guests.tsx
+++ web/src/pages/trip-details/guests.tsx
@@ -1,4 +1,5 @@
 import { Link2, Plus } from "lucide-react";
+import { Button } from "../../components/button";
 
 export function Guests() {
   return (
@@ -22,10 +23,10 @@ export function Guests() {
         </div>
       </div>
 
-      <button className='bg-zinc-800 w-full justify-center text-zinc-200 rounded-lg px-5 h-11 font-medium flex items-center gap-2 hover:bg-zinc-700'>
+      <Button variant="secondary" size="full">
         <Plus className='size-5' />
         Cadastrar novo link
-      </button>
+      </Button>
     </div>
   )
 }
