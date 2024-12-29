6aae3a27e62ec2b67af190362603d50a91d2429c
KesseT <64742407+KetCode@users.noreply.github.com>
2024-12-29 16:30:35 -0300

    feat: create trip with backend

--- web/src/pages/create-trip/steps/destination-date-step.tsx
+++ web/src/pages/create-trip/steps/destination-date-step.tsx
@@ -7,13 +7,15 @@ import 'react-day-picker/dist/style.css'
 
 interface DestinationDateStepProps {
   isGuestsInputOpen: boolean
+  eventStartAndEndDates: DateRange | undefined
   closeGuestsInput: () => void
   openGuestsInput: () => void
+  setDestination: (destination: string) => void
+  setEventStartAndEndDates: (dates: DateRange | undefined) => void
 }
 
-export function DestinationDateStep({ isGuestsInputOpen, closeGuestsInput, openGuestsInput }: DestinationDateStepProps) {
+export function DestinationDateStep({ isGuestsInputOpen, closeGuestsInput, openGuestsInput, setDestination, eventStartAndEndDates, setEventStartAndEndDates }: DestinationDateStepProps) {
   const [isDatePickerOpen, setIsDatePickerOpen] = useState(false)
-  const [eventStartAndEndDates, setEventStartAndEndDates] = useState<DateRange | undefined>()
 
   function openDatePicker() {
     return setIsDatePickerOpen(true)
@@ -30,7 +32,7 @@ export function DestinationDateStep({ isGuestsInputOpen, closeGuestsInput, openG
     <div className="h-16 bg-zinc-900 px-4 rounded-xl flex items-center shadow-shape gap-3">
       <div className="flex items-center gap-2 flex-1">
         <MapPin className='size-5 text-zinc-400' />
-        <input disabled={isGuestsInputOpen} type="text" placeholder="Para onde você vai?" className="bg-transparent text-lg placeholder-zinc-400 outline-none flex-1" />
+        <input disabled={isGuestsInputOpen} onChange={event => setDestination(event.target.value)} type="text" placeholder="Para onde você vai?" className="bg-transparent text-lg placeholder-zinc-400 outline-none flex-1" />
       </div>
 
       <button  onClick={openDatePicker} disabled={isGuestsInputOpen} className="flex items-center gap-2 text-left w-60">
