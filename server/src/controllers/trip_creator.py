8905b212096dc619b0bf35227d47c1b33a91d7e2
KesseT <64742407+KetCode@users.noreply.github.com>
2025-01-01 14:34:03 -0300

    fix: creating the participants in the appropriate table

--- server/src/controllers/trip_creator.py
+++ server/src/controllers/trip_creator.py
@@ -7,23 +7,38 @@ from src.templates.email import email_template
 from src.main.server.config import PORT
 
 class TripCreator:
-    def __init__(self, trip_repository, emails_repository) -> None:
+    def __init__(self, trip_repository, participants_repository) -> None:
         self.__trip_repository = trip_repository
-        self.__emails_repository = emails_repository
+        self.__participants_repository = participants_repository
 
     def create(self, body) -> Dict:
         try:
             emails = body.get("emails_to_invite")
+            owner_name = body.get("owner_name")
+            owner_email = body.get("owner_email")
 
             trip_id = str(uuid.uuid4())
             trip_infos = { **body, "id": trip_id }
 
             self.__trip_repository.create_trip(trip_infos)
 
+            if owner_name:
+                self.__participants_repository.registry_participants({
+                    "name": owner_name,
+                    "email": owner_email,
+                    "is_confirmed": 1,
+                    "is_owner": 1,
+                    "trip_id": trip_id,
+                    "id": str(uuid.uuid4()),
+                })
+
             if emails:
                 for email in emails:
-                    self.__emails_repository.registry_email({
+                    self.__participants_repository.registry_participants({
+                        "name": None,
                         "email": email,
+                        "is_confirmed": 0,
+                        "is_owner": 0,
                         "trip_id": trip_id,
                         "id": str(uuid.uuid4())
                     })
