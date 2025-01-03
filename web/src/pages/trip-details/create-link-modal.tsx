2b6cbd8f9d3b3ec78e1ecde91c6c548e762d0e35
KesseT <64742407+KetCode@users.noreply.github.com>
2025-01-02 22:14:44 -0300

    new: create link modal

---
+++ web/src/pages/trip-details/create-link-modal.tsx
@@ -0,0 +1,40 @@
+import { Link2, Tag } from "lucide-react";
+import { Modal } from "../../components/modal";
+import { useParams } from "react-router-dom";
+import { api } from "../../lib/axios";
+import { FormEvent } from "react";
+
+interface CreateLinkModalProps {
+  closeCreateLinkModal: () => void
+}
+
+export function CreateLinkModal({ closeCreateLinkModal }: CreateLinkModalProps) {
+  const { tripId } = useParams()
+
+  async function createLink(event: FormEvent<HTMLFormElement>) {
+    event.preventDefault()
+
+    const data = new FormData(event.currentTarget)
+
+    const title = data.get('title')?.toString()
+    const url = data.get('url')?.toString()
+
+    await api.post(`/trips/${tripId}/link`, { title, url })
+
+    location.reload();
+  }
+
+  return (
+    <Modal title="Cadastrar link" description="Todos convidados podem visualizar os links importantes." buttonText="Salvar link" closeButton={closeCreateLinkModal} submitButton={createLink}>
+      <div className='px-4 h-14 bg-zinc-950 border border-zinc-800 rounded-lg flex items-center gap-2'>
+        <Tag className='text-zinc-400 size-5' />
+        <input name='title' placeholder="Título do link" className="bg-transparent text-lg placeholder-zinc-400 outline-none flex-1" />
+      </div>
+
+      <div className='px-4 h-14 bg-zinc-950 border border-zinc-800 rounded-lg flex items-center gap-2'>
+        <Link2 className='text-zinc-400 size-5' />
+        <input name='url' placeholder="URL" className="bg-transparent text-lg placeholder-zinc-400 outline-none flex-1" />
+      </div>
+    </Modal>
+  )
+}
