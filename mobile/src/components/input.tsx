33ea568315d2a88af5b2d7d4dffa4f067230bb6f
KesseT <64742407+KetCode@users.noreply.github.com>
2025-01-09 00:21:39 -0300

    fix: input height and format document

--- mobile/src/components/input.tsx
+++ mobile/src/components/input.tsx
@@ -11,21 +11,23 @@ type InputProps = ViewProps & {
     variant?: Variants
 }
 
-function Input({children, variant = "primary", className, ...rest} : InputProps) {
+function Input({ children, variant = "primary", className, ...rest }: InputProps) {
     return (
         <View className={clsx(
-            "h-12 flex-row items-center gap-2",
-            {"h-12 px-4 rounded-lg border border-zinc-800": variant !== "primary",
-            "bg-zinc-950": variant === "secondary",
-            "bg-zinc-900": variant === "tertiary",},
+            "h-16 flex-row items-center gap-2",
+            {
+                "h-14 px-4 rounded-lg border border-zinc-800": variant !== "primary",
+                "bg-zinc-950": variant === "secondary",
+                "bg-zinc-900": variant === "tertiary",
+            },
             className
-        )} {...rest}> 
-            {children} 
+        )} {...rest}>
+            {children}
         </View>)
 }
 
-function Field({...rest} : TextInputProps) {
-    return <TextInput className="flex-1 text-zinc-100" placeholderTextColor={colors.zinc[400]} cursorColor={colors.zinc[100]} selectionColor={Platform.OS === "ios" ? colors.zinc[100] : undefined} {...rest}/>
+function Field({ ...rest }: TextInputProps) {
+    return <TextInput className="flex-1 text-zinc-100" placeholderTextColor={colors.zinc[400]} cursorColor={colors.zinc[100]} selectionColor={Platform.OS === "ios" ? colors.zinc[100] : undefined} {...rest} />
 }
 
 Input.Field = Field
