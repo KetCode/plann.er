cc7e3ab26a4eceedc2c0bd9d9b4045c4fd466751
KesseT <64742407+KetCode@users.noreply.github.com>
2025-01-14 09:41:38 -0300

    imp: error message and format document

--- web/src/pages/create-trip/confirm-trip-modal.tsx
+++ web/src/pages/create-trip/confirm-trip-modal.tsx
@@ -12,24 +12,25 @@ interface ConfirmTripModalProps {
   setOwnerName: (name: string) => void
   setOwnerEmail: (email: string) => void
   createTrip: (event: FormEvent<HTMLFormElement>) => void
+  error: string | null | undefined
 }
 
-export function ConfirmTripModal({ closeConfirmTripModal, createTrip, destination, starts_at, ends_at, setOwnerName, setOwnerEmail }: ConfirmTripModalProps) {
-  
+export function ConfirmTripModal({ closeConfirmTripModal, createTrip, destination, starts_at, ends_at, setOwnerName, setOwnerEmail, error }: ConfirmTripModalProps) {
+
   const displayedDate = format(starts_at as Date, "d").concat(' a ').concat(format(ends_at as Date, "d' de 'LLLL' de 'yyyy", { locale: ptBR }))
 
   return (
-    <Modal 
-      title="Confirmar criação da viagem" 
+    <Modal
+      title="Confirmar criação da viagem"
       description={
-        <> 
-        Para concluir a criação da viagem para <span className="font-semibold text-zinc-100">{destination}</span> nas datas de <span className="font-semibold text-zinc-100">{displayedDate}</span> preencha seus dados abaixo: 
+        <>
+          Para concluir a criação da viagem para <span className="font-semibold text-zinc-100">{destination}</span> nas datas de <span className="font-semibold text-zinc-100">{displayedDate}</span> preencha seus dados abaixo:
         </>
-      } 
-      buttonText="Confirmar criação da viagem" 
-      closeButton={closeConfirmTripModal} 
+      }
+      buttonText="Confirmar criação da viagem"
+      closeButton={closeConfirmTripModal}
       submitButton={createTrip}
-      >
+    >
       <div className='px-4 h-14 bg-zinc-950 border border-zinc-800 rounded-lg flex items-center gap-2'>
         <User className='text-zinc-400 size-5' />
         <input name='name' onChange={event => setOwnerName(event.target.value)} placeholder="Seu nome completo" className="bg-transparent text-lg placeholder-zinc-400 outline-none flex-1" />
@@ -39,6 +40,8 @@ export function ConfirmTripModal({ closeConfirmTripModal, createTrip, destinatio
         <Mail className='text-zinc-400 size-5' />
         <input type="email" onChange={event => setOwnerEmail(event.target.value)} name='email' placeholder="Seu email pessoal" className="bg-transparent text-lg placeholder-zinc-400 outline-none flex-1" />
       </div>
+
+      {error && <p className="text-red-700 text-sm mt-2">{error}</p>}
     </Modal>
   )
 }
